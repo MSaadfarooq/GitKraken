4e0b4b0530b1f7d5d3bd87898140474187de9c28
saadM <mtgenomics@gmail.com>
2025-02-28 14:47:55 +0500

    moved source file to code folder and snplet edited

--- code/scriptlets.R
+++ code/scriptlets.R
@@ -2,15 +2,22 @@
 
 # Put comments here
 #  including pseudo-code of program flow
-myString <- "Hello, World!"
-
-print (myString)
-
 # Put globals, installs, and sources here
+source("./util_functions.R")
 
 # Put functions here
+#   1. Prints hello world
+hello_world <- function() {
+  myString <- "Hello, World!"
+  print (myString)
+}
 
-# now your main code
-can edit
+# main code
+
+hello_world()
+
+square_it(10)
+
+montecarloPi(3000)
 # END
 
